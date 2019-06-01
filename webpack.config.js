9efdb37e57c1beb4dd3e8bc774b33b560a180b08
Ahmed Mohsen <xa.mohsen@gmail.com>
2019-06-01 16:54:56 +0200

    fix image issue

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
     rules: [
       {test: /\.(js)$/, use: 'babel-loader'},
       {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {
+        test: /\.(png|jp(e*)g|svg|ico)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 800000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
+      }
     ],
   },
   mode: 'development',
